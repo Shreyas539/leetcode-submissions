9ea522d7b50ba7160b38e18394f5cb7851bfeaf0
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-05-19 11:32:21 +0530

    Time: 3 ms (86.76%) | Memory: 52.5 MB (33.06%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,37 @@
+class Solution {
+    int[][] dirs = {{-1,0},{0,-1},{1,0},{0,1}};
+
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int cnt=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1'){
+                    cnt++;
+                    bfs(grid,i,j);
+                }
+            }
+        }    
+        return cnt;
+    }
+
+    private void bfs(char[][] grid,int row,int col){
+        if(grid[row][col]=='0') return;
+        char temp = grid[row][col];
+        grid[row][col] = '0';
+
+        for(int[] dir:dirs){
+            int nr = row+dir[0];
+            int nc = col+dir[1];
+
+            if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]=='1'){
+                
+                bfs(grid,nr,nc);
+            }
+
+        }
+
+        
+    }
+}
