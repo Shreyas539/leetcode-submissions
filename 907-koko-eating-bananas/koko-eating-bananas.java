8248778dace518c32b3542e1eb1c19b62d492f60
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-05-25 21:56:35 +0530

    Time: 9 ms (60.83%) | Memory: 47.7 MB (85.11%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -2,7 +2,7 @@ class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int l = 1;
         int r= Arrays.stream(piles).max().getAsInt();
-        int ans=r;
+        int ans=l;
         while(l<=r){
             int mid = l+(r-l)/2;
             if(canEatAll(mid,piles,h)){
