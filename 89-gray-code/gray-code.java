64a1ee3807f5e20ba360aa9de8a23f0c1e9a0188
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-05-30 23:29:07 +0530

    Time: 4 ms (99.46%) | Memory: 53.3 MB (92.40%) - LeetSync

---
+++ 89-gray-code/gray-code.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        
+        int len = 1<<n;
+        for(int i=0;i<len;i++){
+            int num = i ^ (i>>1);
+           
+                res.add(num);
+            
+        }
+        return res;
+    }
+}
