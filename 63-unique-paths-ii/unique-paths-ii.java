c5d25ca55825cf6bb8f85346542880505ef5bb8d
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-06-09 22:54:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (26.01%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -33,13 +33,7 @@ class Solution {
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(dp[i][j]+" ");
-            }
-            System.out.println();
-
-        }
+        
         return dp[m-1][n-1];
     }
 }
