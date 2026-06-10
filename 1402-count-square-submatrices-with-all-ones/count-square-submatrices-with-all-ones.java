c4393d09ad0e8adeceff759c0d05eb2b98655303
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-06-10 22:22:15 +0530

    Time: 6 ms (86.24%) | Memory: 61.9 MB (37.87%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -4,40 +4,23 @@ class Solution {
         int n = matrix[0].length;
         int[][] dp = new int[m][n];
 
-        for(int i=0;i<m;i++){
-            if(matrix[i][0]==0){
-                dp[i][0]=0;
-            }else{
-                dp[i][0] = 1;
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(matrix[0][i]==0){
-                dp[0][i]=0;
-            }else{
-                dp[0][i] = 1;
-            }
-        }
+        int total = 0;
 
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(matrix[i][j]!=0){
-
-                dp[i][j] = 1 + Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1; // first row/col
+                    } else {
+                        dp[i][j] = 1 + Math.min(
+                            dp[i-1][j], 
+                            Math.min(dp[i][j-1], dp[i-1][j-1])
+                        );
+                    }
+                    total += dp[i][j]; // add all squares ending here
                 }
             }
         }
-        int ans = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]!=0){
-
-                ans+=dp[i][j];
-                }
-                
-            }
-            
-        }
-        return ans;
+        return total;
     }
 }
