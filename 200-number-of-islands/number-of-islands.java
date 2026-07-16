c2b44bfb3a3f62fb62e649d669f5dfe3365c06ac
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-07-16 22:59:37 +0530

    Time: 3 ms (87.99%) | Memory: 52.6 MB (14.86%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,46 +1,33 @@
 class Solution {
-    int[][] dirs = {{-1,0},{0,-1},{1,0},{0,1}};
+        int res=0;
+        int[][] dir = {{-1,0},{0,-1},{1,0},{0,1}};
 
     public int numIslands(char[][] grid) {
+        
         int m = grid.length;
         int n = grid[0].length;
-        int cnt=0;
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    dfs(grid,i,j);
-                    cnt++;
+                    res++;
+                    bfs(grid,i,j,m,n);
                 }
-            }
-        }    
-        return cnt;
-    }
-
-    private void dfs(char[][] grid,int row,int col){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{row,col});
-        grid[row][col]='#';
-
-        while(!q.isEmpty()){
-            
-
-            int[] cur = q.poll();
-            int r = cur[0];
-            int c = cur[1];
-
-            for(int[] dir:dirs){
-
-                int nr = r+dir[0];
-                int nc = c+dir[1];
-
-                if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]=='1'){
-                    grid[nr][nc]='#';
-                    q.offer(new int[]{nr,nc});                    
-                }
-
             }
         }
+        return res;
+    }
+    private void bfs(char[][] grid,int row,int col,int m,int n){
+        if(row<0 || col<0 || row>=m || col>=n || grid[row][col]=='0'){
+            return;
+        }
 
-       
+        grid[row][col]='0';
+
+        for(int[] d:dir){
+            int nr = row+d[0];
+            int nc = col+d[1];
+            bfs(grid,nr,nc,m,n);
+        }
     }
 }
