25b495ec820ad5108d00ecedae16d4e361e5693a
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-07-17 23:57:23 +0530

    Time: 13 ms (82.32%) | Memory: 47.8 MB (46.56%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,40 +1,34 @@
 class Solution {
-    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
-    
+    int[][] dir = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
     public int shortestPathBinaryMatrix(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
 if (grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+
         Queue<int[]> q = new LinkedList<>();
         q.offer(new int[]{0,0,1});
-        grid[0][0] = 1;
 
         while(!q.isEmpty()){
-            
-            int[] cur = q.poll();
-            int r = cur[0];
-            int c = cur[1];
-            int dist = cur[2];
+            int size = q.size();
 
-            if(r==m-1 && c==n-1) return dist;
+            for(int i=0;i<size;i++){
+                int[] cur = q.poll();
+                int r = cur[0];
+                int c = cur[1];
+                int dist = cur[2];
+               if(r==m-1 && c==n-1) return dist;
 
-            for(int[] dir:dirs){
-                int nr = r+dir[0];
-                int nc = c+dir[1];
-                
-                
+                for(int[] d:dir){
+                    int nr = r+d[0];
+                    int nc = c+d[1];
 
-                if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==0){
-                    grid[nr][nc]=1;
-                    q.offer(new int[]{nr,nc,dist+1});
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==0){
+                        grid[nr][nc]=1;
+                        q.offer(new int[]{nr,nc,dist+1});
+                    }
                 }
             }
         }
-
         return -1;
-
-        
-        
     }
-    
 }
