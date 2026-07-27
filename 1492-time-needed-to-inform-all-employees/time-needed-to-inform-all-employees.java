14d432d2216e76193844ce46174770e1514982e7
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-07-27 22:58:53 +0530

    Time: 58 ms (59.02%) | Memory: 125.1 MB (62.19%) - LeetSync

---
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.java
@@ -0,0 +1,32 @@
+class Solution {
+    int ans=0;
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<n;i++){
+            if(manager[i]!=-1){
+                adj.get(manager[i]).add(i);
+            }
+        }
+        
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{headID,0});
+        int maxtime = 0;
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int id= cur[0];
+            int time = cur[1];
+
+            maxtime = Math.max(maxtime,time);
+
+            for(int emp:adj.get(id)){
+                q.offer(new int[]{emp,time+informTime[id]});
+            }
+        }
+        return maxtime;
+    }
+}
