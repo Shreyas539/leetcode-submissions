0ded8d7b92d9d40e57b1d0301a7f0d3dfb9f6f12
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-07-28 22:41:36 +0530

    Time: 3 ms (26.08%) | Memory: 48.3 MB (7.16%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.java
@@ -20,15 +20,17 @@ class Solution {
         cur.add(node);
         if(node==n-1){
             res.add(new ArrayList<>(cur));
-            return;
+            
+        }else{
+            for(int nei:adj.get(node)){
+                
+                bc(adj,res,nei,cur,n);
+            }
+
         }
+        cur.remove(cur.size()-1);
 
         
 
-        for(int nei:adj.get(node)){
-            
-            bc(adj,res,nei,cur,n);
-            cur.remove(cur.size()-1);
-        }
     }
 }
