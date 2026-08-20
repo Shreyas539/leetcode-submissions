4401e643725672aabb0d613a4a11ac5d14a36a99
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-08-20 14:46:11 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (38.38%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.java
@@ -0,0 +1,32 @@
+class Solution {
+    int[] parent;
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        parent  = new int[n+1];
+
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+        }
+
+        for(int[] edge:edges){
+            if(union(edge[0],edge[1])){
+                return new int[]{edge[0],edge[1]};
+            }
+        }
+        return new int[]{};
+    }
+    private boolean union(int a,int b){
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if(rootA==rootB) return true;
+        parent[rootB] = rootA;
+        return false;
+    }
+    private int find(int x){
+        if(parent[x]==x){
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+}
