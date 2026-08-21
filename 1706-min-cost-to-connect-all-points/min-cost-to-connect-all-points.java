d0a343be57beb930d49dbdce8c0cb187849aa2ec
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-08-21 14:05:35 +0530

    Time: 321 ms (47.54%) | Memory: 98.4 MB (27.12%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -0,0 +1,49 @@
+class Solution {
+    int[] parent;
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        parent = new int[n];
+        List<int[]> edges = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int dist = Math.abs(points[i][0]-points[j][0]) + Math.abs(points[i][1]-points[j][1]);
+                edges.add(new int[]{dist,i,j});
+            }
+        }
+
+        edges.sort((a,b)->a[0]-b[0]);
+        int cost=0,cnt=0;
+        for(int[] e:edges){
+            int wt = e[0],u=e[1],v=e[2];
+
+            if(union(u,v)){
+                cost+=wt;
+                cnt++;
+                if(cnt==n-1) break;
+            }
+        }
+        return cost;
+    }
+    private boolean union(int a,int b){
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if(rootA==rootB){
+            return false;
+        }
+
+        parent[rootB]=rootA;
+
+        return true;
+    }
+    private int find(int x){
+        if(parent[x]==x){
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+}
