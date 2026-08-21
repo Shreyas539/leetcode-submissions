95fe718848d46a4a52003e815b7cf9f83f37f048
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-08-21 12:10:57 +0530

    Time: 1 ms (86.54%) | Memory: 47.2 MB (74.63%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -10,7 +10,7 @@ class Solution {
         }    
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+            for(int j=i+1;j<n;j++){
 
                 if(isConnected[i][j]==1){
 
