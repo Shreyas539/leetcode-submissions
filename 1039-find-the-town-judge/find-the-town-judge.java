fc3094a466c4cb18bb6de54a2c964d587c3790c5
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-09-03 23:23:20 +0530

    Time: 3 ms (70.02%) | Memory: 53.8 MB (19.39%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,15 +1,16 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int[] ind = new int[n+1];
-        int[] od = new int[n+1];
+        int[] inDegree = new int[n+1];
+        int[] outDegree = new int[n+1];
 
-        for(int[] i:trust){
-            od[i[0]]++;
-            ind[i[1]]++;
+        for (int[] t : trust) {
+            int a = t[0], b = t[1];
+            outDegree[a]++;
+            inDegree[b]++;
         }
 
-        for(int i=1;i<=n;i++){
-            if(ind[i]==n-1 && od[i]==0){
+        for (int i = 1; i <= n; i++) {
+            if (inDegree[i] == n-1 && outDegree[i] == 0) {
                 return i;
             }
         }
