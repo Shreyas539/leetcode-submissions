2226391cef10eadd1e3ca5f7b2d0e1e721e5e1ce
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-09-11 11:11:24 +0530

    Time: 1 ms (87.05%) | Memory: 44 MB (74.64%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,16 +1,17 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        bc(0,res,nums,new ArrayList<>());
+        backtrack(nums,res,new ArrayList<>(),0);
         return res;
     }
-    private void bc(int start,List<List<Integer>> res,int[] nums,List<Integer> cur){
+    private void backtrack(int[] nums,List<List<Integer>> res,List<Integer> cur,int idx){
         res.add(new ArrayList<>(cur));
 
-        for(int i=start;i<nums.length;i++){
+        for(int i=idx;i<nums.length;i++){
             cur.add(nums[i]);
-            bc(i+1,res,nums,cur);
+            backtrack(nums,res,cur,i+1);
             cur.remove(cur.size()-1);
         }
     }
+    
 }
