880ec0cc162c2c9a0e76e368949addaf6b5c6581
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-09-15 12:24:45 +0530

    Time: 1 ms (87.97%) | Memory: 44.7 MB (99.78%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,10 +1,11 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        backtrack(res,new ArrayList<>(),nums,new boolean[nums.length]);
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums,res,new ArrayList<>(),0,used);
         return res;
     }
-    private void backtrack(List<List<Integer>> res,List<Integer> cur,int[] nums,boolean[] used){
+    private void backtrack(int[] nums,List<List<Integer>> res,List<Integer> cur,int idx,boolean[] used){
         if(cur.size()==nums.length){
             res.add(new ArrayList<>(cur));
             return;
@@ -12,11 +13,12 @@ class Solution {
 
         for(int i=0;i<nums.length;i++){
             if(used[i]) continue;
-            used[i] = true;
+            used[i]=true;
             cur.add(nums[i]);
-            backtrack(res,cur,nums,used);
+            backtrack(nums,res,cur,idx+1,used);
             cur.remove(cur.size()-1);
             used[i]=false;
         }
+
     }
 }
