504b7268fd8f26894912d6de7680ab7e68901d18
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-09-16 12:18:04 +0530

    Time: 2 ms (94.27%) | Memory: 45.9 MB (33.02%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,19 +1,19 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res= new ArrayList<>();
-
-        backtrack(candidates,0,new ArrayList<>(),res,target);
+    public List<List<Integer>> combinationSum(int[] cand, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(cand,target,res,new ArrayList<>(),0);
         return res;
     }
-    private void backtrack(int[] cand,int idx,List<Integer> cur,List<List<Integer>> res,int remain){
-        if(remain==0){
+    private void backtrack(int[] cand,int target,List<List<Integer>> res,List<Integer> cur,int idx){
+        if(target==0){
             res.add(new ArrayList<>(cur));
+            return;
         }
-        if(remain<0)return;
+        if(target<0) return;
         for(int i=idx;i<cand.length;i++){
             cur.add(cand[i]);
-            backtrack(cand,i,cur,res,remain-cand[i]);
+            backtrack(cand,target-cand[i],res,cur,i);
             cur.remove(cur.size()-1);
         }
-    } 
+    }
 }
