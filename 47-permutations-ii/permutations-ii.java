6456bf64f556881f294436454c9c585355f42763
react-flow <131728495+Shreyas539@users.noreply.github.com>
2026-09-16 11:52:43 +0530

    Time: 3 ms (93.10%) | Memory: 46.7 MB (42.63%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -2,21 +2,23 @@ class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        bc(res,new ArrayList<>(),nums,new boolean[nums.length]);
+        boolean[] used  = new boolean[nums.length];
+        backtrack(nums,res,new ArrayList<>(),used);
         return res;
     }
-    private void bc(List<List<Integer>> res,List<Integer> cur,int[] nums,boolean[] used){
+    private void backtrack(int[] nums,List<List<Integer>> res,List<Integer> cur,boolean[] used){
         if(cur.size()==nums.length){
             res.add(new ArrayList<>(cur));
+            return;
         }
 
         for(int i=0;i<nums.length;i++){
             if(used[i]) continue;
             if(i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
+
             used[i]=true;
             cur.add(nums[i]);
-            bc(res,cur,nums,used);
-
+            backtrack(nums,res,cur,used);
             cur.remove(cur.size()-1);
             used[i]=false;
         }
